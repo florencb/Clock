7ba444a6f82925e47f5994c8ea88995b73892f4f
florencb <bradjflorence@gmail.com>
2016-04-08 13:00:54 -0400

    initial commit

---
+++ frmClock.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmClock
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblTimeDisplay = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'lblTimeDisplay
+        '
+        Me.lblTimeDisplay.AutoSize = True
+        Me.lblTimeDisplay.Font = New System.Drawing.Font("Footlight MT Light", 32.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTimeDisplay.ForeColor = System.Drawing.Color.YellowGreen
+        Me.lblTimeDisplay.Location = New System.Drawing.Point(27, 33)
+        Me.lblTimeDisplay.Name = "lblTimeDisplay"
+        Me.lblTimeDisplay.Size = New System.Drawing.Size(137, 46)
+        Me.lblTimeDisplay.TabIndex = 0
+        Me.lblTimeDisplay.Text = "Label1"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'frmClock
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(285, 113)
+        Me.Controls.Add(Me.lblTimeDisplay)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "frmClock"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "My Clock"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblTimeDisplay As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ frmClock.vb
@@ -0,0 +1,14 @@
+﻿Public Class frmClock
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        lblTimeDisplay.Text = TimeOfDay.ToLongTimeString()
+    End Sub
+
+    Private Sub lblTimeDisplay_Click(sender As Object, e As EventArgs) Handles lblTimeDisplay.Click
+
+    End Sub
+
+    Private Sub frmClock_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "My Clock"
+    End Sub
+End Class
